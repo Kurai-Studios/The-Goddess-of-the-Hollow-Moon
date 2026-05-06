d6b713d2c2d573559e5843f5020de16e4c17e1d4
Kurai <fxt.kurai@gmail.com>
2026-05-05 20:44:39 -0400

    Base Animations
    Added base animations for idle and walking

--- Assets/_Scripts/_Player/PlayerMovement.cs
+++ Assets/_Scripts/_Player/PlayerMovement.cs
@@ -7,11 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
+
     private Vector2 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -21,6 +24,17 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
